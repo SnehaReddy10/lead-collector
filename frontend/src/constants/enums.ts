739a386606b656d18659d35a3df442ee934944f0
Sneha Reddy <snehareddy9178@gmail.com>
2024-12-08 00:49:33 +0530

    feat: add constants

---
+++ frontend/src/constants/enums.ts
@@ -0,0 +1,10 @@
+export enum InteractionType {
+  SIGNUP = 'signup',
+  LOGIN = 'login',
+  COURSES = 'courses',
+  ABOUT = 'about',
+  ENROLL_COURSE = 'enroll_course',
+  PURCHASE_COURSE = 'purchase_course',
+  SUBSCRIBE = 'subscribe',
+  INTEREST_COURSE = 'interest_course',
+}
